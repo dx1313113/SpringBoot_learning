0302c8af5144e0d425909c6a12c5e21503c1691d
dx1313113 <dxin113@163.com>
2023-10-08 21:55:22 +0800

    实体类

---
+++ src/main/java/com/whgcdx/demo1/entity/SysUserEntity.java
@@ -0,0 +1,13 @@
+package com.whgcdx.demo1.entity;
+
+/**
+ * @projectName: demo1
+ * @package: com.whgcdx.demo1.entity
+ * @className: SysUserEntity
+ * @author: dx
+ * @description: TODO
+ * @date: 2023/10/8 21:36
+ * @version: 1.0
+ */
+public class SysUserEntity {
+}
