47aaa38a5dda696560bec09696785a232b0772b5
dx1313113 <dxin113@163.com>
2023-10-09 15:21:43 +0800

    实体

--- src/main/java/com/whgcdx/demo1/entity/SysUserEntity.java
+++ src/main/java/com/whgcdx/demo1/entity/SysUserEntity.java
@@ -1,5 +1,12 @@
 package com.whgcdx.demo1.entity;
 
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.whgcdx.demo1.common.entity.BaseEntity;
+import lombok.Data;
+
+import java.io.Serializable;
+
 /**
  * @projectName: demo1
  * @package: com.whgcdx.demo1.entity
@@ -9,5 +16,21 @@ package com.whgcdx.demo1.entity;
  * @date: 2023/10/8 21:36
  * @version: 1.0
  */
-public class SysUserEntity {
+
+@Data
+@TableName("sys_user")
+public class SysUserEntity extends BaseEntity implements Serializable {
+
+    private String userName;
+
+    private String realName;
+
+    private String pwd;
+
+    private Integer mobile;
+
+    private String email;
+
+    private String salt;
+
 }
