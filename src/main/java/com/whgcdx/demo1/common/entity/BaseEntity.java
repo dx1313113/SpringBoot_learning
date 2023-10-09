d97ea55e091be8d77f7fe917349cc182c9a89ff7
dx1313113 <dxin113@163.com>
2023-10-09 15:09:54 +0800

    基本实体类

---
+++ src/main/java/com/whgcdx/demo1/common/entity/BaseEntity.java
@@ -0,0 +1,27 @@
+package com.whgcdx.demo1.common.entity;
+
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Data;
+
+/**
+ * @projectName: demo1
+ * @package: com.whgcdx.demo1.common.entity
+ * @className: BaseEntity
+ * @author: dx
+ * @description: TODO
+ * @date: 2023/10/8 22:16
+ * @version: 1.0
+ */
+
+@Data
+public class BaseEntity {
+
+    @TableId
+    protected Integer id;
+
+    protected Integer createAt;
+
+    protected Integer updateAt;
+
+    protected Integer deleteAt;
+}
