607a0f492ee1a806409569812d2adbb0e30d1e0f
dx1313113 <dxin113@163.com>
2023-10-10 21:19:28 +0800

    swagger配置

---
+++ src/main/java/com/whgcdx/demo1/config/swagger/SwaggerConfig.java
@@ -0,0 +1,52 @@
+package com.whgcdx.demo1.config.swagger;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ReflectionUtils;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfoHandlerMapping;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.spring.web.plugins.WebMvcRequestHandlerProvider;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @projectName: demo1
+ * @package: com.whgcdx.demo1.config.swagger
+ * @className: SwaggerConfig
+ * @author: dx
+ * @description: TODO swagger配置文档
+ * @date: 2023/10/10 16:56
+ * @version: 1.0
+ */
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.whgcdx.demo1.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("接口文档")
+                .description("接口文档")
+                .version("1.0.0")
+                .build();
+    }
+}
